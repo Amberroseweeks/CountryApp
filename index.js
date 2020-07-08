0bc5244236e8a7b6841059f7c5f356062ef2c032
rarwrly <ehc2642@gmail.com>
2020-07-07 20:57:36 -0400

    working buttons

--- index.js
+++ index.js
@@ -68,21 +68,67 @@ function displayCountryData (data) {
         flag.style.backgroundSize = "100% 100%"
         countryContainer.appendChild(flag)
         flag.setAttribute("class", "image-container")
+    
+    let translations = document.createElement('div')
+        countryContainer.appendChild(translations)
+        translations.setAttribute("class", "info")
         
-    let translationDE = document.createElement('div')
+    let translationsButtonDE= document.createElement("BUTTON");
+    let translationsButtontxtDE=document.createTextNode('DE');
+        translations.appendChild(translationsButtonDE)
+        translationsButtonDE.appendChild(translationsButtontxtDE)
+        translationsButtonDE.setAttribute("id","DE")
+
+        translationsButtonDE.addEventListener("click",function (){
+                let translationDE = document.createElement('div')
         translationDE.innerHTML = "DE: " + data[i].translations.de
         countryContainer.appendChild(translationDE)
         translationDE.setAttribute("class", "info")
+            
+        }
         
-    let translationFR = document.createElement('div')
+        )
+    let translationsButtonFR= document.createElement("BUTTON");
+    let translationsButtontxtFR=document.createTextNode('FR');
+        translations.appendChild(translationsButtonFR)
+        translationsButtonFR.appendChild(translationsButtontxtFR)
+        translationsButtonFR.setAttribute("id","FR")
+        
+        translationsButtonFR.addEventListener("click",function(){
+                let translationFR = document.createElement('div')
         translationFR.innerHTML = "FR: " + data[i].translations.fr
         countryContainer.appendChild(translationFR)
         translationFR.setAttribute("class", "info")
+            
+            
+        }
         
-    let translationJA = document.createElement('div')
+        
+        )
+
+    let translationsButtonJA= document.createElement("BUTTON");
+    let translationsButtontxtJA=document.createTextNode('JA');
+        translations.appendChild(translationsButtonJA)
+        translationsButtonJA.appendChild(translationsButtontxtJA)
+        translationsButtonJA.setAttribute("id","JA")
+        
+                translationsButtonJA.addEventListener("click",function(){
+                let translationJA = document.createElement('div')
         translationJA.innerHTML = "JA: " + data[i].translations.ja
         countryContainer.appendChild(translationJA)
         translationJA.setAttribute("class", "info")
+            
+            
+        }
+        
+        
+        )
+
+
+        
+
+        
+
 }
 }
 
