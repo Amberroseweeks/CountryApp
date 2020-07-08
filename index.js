14ff60631240bb993b9647b795bd8a281eb46eaf
Amberroseweeks <weeksamberrose@gmail.com>
2020-07-07 20:41:19 -0400

    adding commas and adjusted css

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-// Build a single page app that shows:
-// a. - whatever country the user inserts printed on the page;
-// b. - the capital of this country;
-// c. - population of this country:
-// d. - any other information that seems interesting to you
-// e. - the flag of this country
-
-
-
-
-//$(document).ready(function() {
-//    
-//    submit.addEventListener("click", searchAlbum)
-//    
-//    function searchAlbum(){
-//        $.ajax({
-//        url: "https://restcountries.eu/rest/v2/all",
-//        data: {s: input.value},
-//        success: function(result) {
-//            console.log(result)
-//        },
-//        error: function() {
-//            console.log("error")
-//        }
-//    })
-//
-//    }
-//                
-//    })
-
-//test edit on Amber's Branch
-
-
 $(document).ready(function() {
 
 
@@ -67,15 +34,17 @@ function displayCountryData (data) {
         countryContainer.appendChild(capitalName)
         capitalName.setAttribute("class", "info")
 
+    let num = data[i].population
+    
+    commaNumber = Number(num.toFixed(0)).toLocaleString().split(/\s/).join(',')
+    console.log(commaNumber)
+        
     let population = document.createElement('div')
-        population.innerHTML = "Population: " + data[i].population
+        population.innerHTML = "Population: " + commaNumber
         countryContainer.appendChild(population)
+        console.log(data[0].population)
         population.setAttribute("class", "info")
         
-    let language = document.createElement('div')
-        language.innerHTML = "Language(s): " + data[i].languages[0].name
-        countryContainer.appendChild(language)
-        language.setAttribute("class", "info")
     
     let flag = document.createElement('div')
         flag.style.width = '250px';
@@ -84,87 +53,10 @@ function displayCountryData (data) {
         flag.style.backgroundSize = "100% 100%"
         countryContainer.appendChild(flag)
         flag.setAttribute("class", "image-container")
+    
         
 }
 }
 
+
 })
-
-
-
-
-
-
-
-//---JUST TO PRINT TO CONSOLE TO CHECK IF IT IS WORKING:
-//$(document).ready(function() {
-//    
-//        $.ajax({
-//        url: "https://restcountries.eu/rest/v2/currency/usd",
-//        success: function(response) {
-//            console.log(response)
-//        },
-//
-//    })
-//                
-//    })
-
-
-
-
-//NOT YET: -----
-
-//$(document).ready( function (){
-//
-//let input = document.getElementById("input")
-//let submit = document.getElementById("submit")
-//
-//
-//let container = document.getElementById("container")
-//
-//submit.addEventListener("click", getCountry)
-//
-//function getCountry() {
-//	$.ajax({
-//		url: "https://restcountries.eu/rest/v2/all",
-//		data: {s: input.value},
-//		success: function(result) {
-//			listResults(result)
-//			listPosters(result)
-//		}
-//	})	
-//}
-//	function listResults (movie) {
-//		let firstResult = movie.Search[0]
-//		$("#title").html(firstResult.Title)
-//		$("#year").html(firstResult.Year)
-//		$("#poster-main").css({
-//			"background-image": "url(" + firstResult.currencies + ")",
-//			"background-size": "100% 100%",
-//			border: "3px solid red"
-//		})
-//	}
-//
-//	function listPosters(movies) {
-//	console.log(movies)	
-//		for(let i = 0; i < movies.Search.length; i++) {
-//				let results = "<div class='poster'><img src='" + movies.Search[i].Poster + "'></div>"
-//				container.innerHTML += results;
-//		}
-//		linking(movies)
-//	}
-//
-//	function linking(movies) {
-//		let poster = document.getElementsByClassName("poster")
-//		for(let i = 0; i < poster.length; i++) {
-//			poster[i].addEventListener("click", function () {
-//				window.open("https://www.imdb.com/title/" + movies.Search[i].imdbID)
-//			})
-//		}
-//					
-//	}
-//
-//
-//
-//
-//})
